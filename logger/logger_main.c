4cc5401b4b351a8f90e22cf4ef3a8d049f877c7b
chibiegg <chibiegg@chibiegg.net>
2019-05-02 03:44:30 +0900

    Using pthread

--- logger/logger_main.c
+++ logger/logger_main.c
@@ -1,7 +1,8 @@
-
 #include <sdk/config.h>
 #include <stdio.h>
 #include "gnss.h"
+
+static void *gnss_loop_task(void *arg);
 
 #ifdef CONFIG_BUILD_KERNEL
 int main(int argc, FAR char *argv[])
@@ -9,8 +10,19 @@ int main(int argc, FAR char *argv[])
 int logger_main(int argc, char *argv[])
 #endif
 {
+  pthread_t thread_gnss;
+
+
   printf("GNSS Logger\n");
   gnss_init();
-  gnss_loop();
+
+  pthread_create(&thread_gnss, NULL, gnss_loop_task, NULL);
+
+  pthread_join(thread_gnss, NULL);
   return 0;
+}
+
+static void *gnss_loop_task(void *arg){
+  gnss_loop();
+  return NULL;
 }
